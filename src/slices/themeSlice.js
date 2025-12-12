ddafa15a5c51e05a0925dd56896f0ebf0776c215
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-12-12 23:38:23 +0530

    Local storage onThemeSlice

--- src/slices/themeSlice.js
+++ src/slices/themeSlice.js
@@ -1,16 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// Load saved theme from localStorage (fallback = light)
+const savedTheme = localStorage.getItem("theme") || "light";
+
 const themeSlice = createSlice({
   name: "theme",
   initialState: {
-    mode: "light", // default
+    mode: savedTheme,
   },
   reducers: {
     toggleTheme: (state) => {
       state.mode = state.mode === "light" ? "dark" : "light";
+
+      // Save to localStorage
+      localStorage.setItem("theme", state.mode);
     },
+
     setTheme: (state, action) => {
       state.mode = action.payload;
+
+      // Save to localStorage
+      localStorage.setItem("theme", action.payload);
     },
   },
 });
