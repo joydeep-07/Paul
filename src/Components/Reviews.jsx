3dfd994ef2b7ee3f4192a9bb54c001f26a2f1bdd
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-12-16 17:57:08 +0530

    Updated Review

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -1,40 +1,80 @@
-import React from 'react'
+import React, { useState } from "react";
+import dp from "../assets/images/dp1.jpg";
 
 const Reviews = () => {
+  const [readMore, setReadMore] = useState(false);
+
+  const text =
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti modi ratione obcaecati! Eligendi excepturi assumenda cumque temporibus recusandae, animi, ducimus repudiandae dignissimos quas enim velit praesentium perspiciatis quisquam dicta. Exercitationem magni aliquam veritatis mollitia modi voluptas numquam dolor? Laborum ratione, debitis non ad quibusdam quaerat neque a laboriosam, animi, fugiat doloribus accusamus ipsam provident sint mollitia enim. Quasi veniam, optio natus illum distinctio facilis reprehenderit blanditiis placeat eos, minus ullam. Nemo officiis qui sunt nam recusandae reprehenderit magnam voluptatibus quasi laboriosam exercitationem ea architecto voluptatem, temporibus doloribus, vitae tenetur molestias illum repudiandae vero. Asperiores debitis libero doloremque quasi velit dolores!";
+
+  const words = text.split(" ");
+  const shortText = words.slice(0, 40).join(" ");
+
   return (
     <>
       <div className="py-15 bg-[var(--bg-main)] flex justify-center items-center">
-        <div className=" w-7xl flex">
+        <div className="w-7xl flex">
+          {/* Left */}
           <div className="w-full md:w-1/3 px-4">
-            {/* Section label */}
             <h3 className="text-xs font-semibold uppercase tracking-[0.3em] text-[var(--text-main)] opacity-70 mb-2">
               Testimonials
             </h3>
-            {/* Decorative line */}
+
             <div className="mt-1 mb-6 h-[2px] w-16 bg-[var(--accent-primary)] rounded-full" />
 
-            {/* Main heading */}
             <h1 className="heading-font text-4xl md:text-5xl leading-tight mb-4 text-[var(--text-heading)]">
-              What others {" "}
+              What others{" "}
               <span className="text-[var(--accent-primary)]">Say</span>
             </h1>
 
-            {/* Description */}
             <p className="text-sm md:text-base leading-relaxed text-[var(--text-main)] opacity-80 max-w-sm">
               I’ve worked with some amazing people over the years here’s what
               they have to say about me.
             </p>
           </div>
 
-          <div className="border w-2/3 flex justify-center items-center px-4 uppercase text-sm text-[var(--text-accent)] opacity-70">
-            right side
+          {/* Right */}
+          <div className=" w-2/3 p-4 border ">
+            <div className="border border-[var(--border-light)] rounded-xl">
+              <div className="flex items-center">
+                <div className="p-2 border-2 border-[var(--accent-primary)] rounded-full m-4">
+                  <img
+                    className="h-16 w-16 rounded-full object-cover"
+                    src={dp}
+                    alt=""
+                  />
+                </div>
+
+                <div>
+                  <h1 className="font-semibold text-[var(--text-main)]">
+                    Debashis Roy
+                  </h1>
+                  <p className="text-sm text-[var(--text-secondary)]">
+                    Senior Software Engineer
+                  </p>
+                </div>
+              </div>
+
+              <p className="text-justify p-4 text-[var(--text-main)] max-w-3xl">
+                {readMore ? text : shortText}
+                {words.length > 40 && (
+                  <span
+                    onClick={() => setReadMore(!readMore)}
+                    className="cursor-pointer text-[var(--accent-primary)] font-medium"
+                  >
+                    {readMore ? " read less" : " ...read more"}
+                  </span>
+                )}
+              </p>
+            </div>
+            Linked in link
           </div>
         </div>
       </div>
 
-      <div className="h-screen bg-[var(--bg-main)] "></div>
+      <div className="h-screen bg-[var(--bg-main)]"></div>
     </>
   );
-}
+};
 
-export default Reviews
+export default Reviews;
