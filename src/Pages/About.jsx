e295761aa5517b95990be7ac6bd25009d829c83c
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-12-17 19:53:24 +0530

    Slider added in About

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,14 +2,30 @@ import React from 'react'
 import Me from '../Components/Me'
 import WorkBadge from "../Components/WorkBadge";
 import Footer from "../layout/Footer";
+import Slide from '../Components/Slide';
+import { SiMongodb, SiExpress, SiTailwindcss, SiFramer } from "react-icons/si";
+import { FaReact, FaNodeJs } from "react-icons/fa";
 const About = () => {
+
+  const skills = [
+      { name: "React", icon: <FaReact /> },
+      { name: "Node.js", icon: <FaNodeJs /> },
+      { name: "MongoDB", icon: <SiMongodb /> },
+      { name: "Express", icon: <SiExpress /> },
+      { name: "Tailwind", icon: <SiTailwindcss /> },
+      { name: "Framer Motion", icon: <SiFramer /> },
+    ];
+
   return (
     <div>
-      <Me/>
-      <WorkBadge/>
-      <Footer/>
+      <Me />
+      <div className='py-20'>
+        <Slide items={skills} speed={20} />
+      </div>
+      <WorkBadge />
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default About
