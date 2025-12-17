6560d9c74ced94edf881080a529b9df73ca5424c
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-12-17 16:02:20 +0530

    Updated

--- src/Utils/Reviews.js
+++ src/Utils/Reviews.js
@@ -1,4 +1,6 @@
-import dp1 from '../assets/images/dp1.jpg'
+import dp1 from "../assets/images/dp1.jpg";
+import dp2 from "../assets/images/dp2.jpg";
+import dp3 from "../assets/images/dp3.jpg";
 
 export const reviews = [
   {
@@ -13,17 +15,16 @@ export const reviews = [
     id: 2,
     name: "Samrat Kundu",
     position: "Sineor Software Engineer",
-    photo: dp1,
+    photo: dp2,
     review:
-      "Really impressive portfolio! The design is clean, user-friendly, and highlights your skills and projects effectively. I especially liked how smoothly the site navigates and how well each section is structured. Great job showcasing your work!",
+      "Really impressive portfolio! The design is clean, user-friendly, and visually appealing. Each section clearly communicates your skills and experience, making it easy to understand your strengths at a glance. I particularly liked the smooth transitions and attention to detail in the layout. It’s evident that a lot of thought has gone into both the functionality and presentation of this portfolio.",
   },
-
   {
     id: 3,
     name: "Prosenjit Chatterjee",
     position: "Sineor Software Engineer",
-    photo: dp1,
+    photo: dp3,
     review:
-      "Really impressive portfolio! The design is clean, user-friendly, and highlights your skills and projects effectively. I especially liked how smoothly the site navigates and how well each section is structured. Great job showcasing your work!",
+      "This is a very well-crafted portfolio with a clean and professional design. The structure is intuitive, and the content flows naturally from one section to the next. I appreciate how clearly the projects are explained and how effectively your technical skills are highlighted. Overall, it’s a strong representation of your work and reflects a solid understanding of both design and development principles.",
   },
 ];
