9c152e993525f8da17a79ce9a63d8a50ad1c1404
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-12-21 20:47:22 +0530

    updated

--- src/Components/ScrollToTop.jsx
+++ src/Components/ScrollToTop.jsx
@@ -8,7 +8,7 @@ const ScrollToTop = () => {
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: "smooth", // use "smooth" if you want animation
+      behavior: "smooth",
     });
   }, [pathname]);
 
