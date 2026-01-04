b50dfe628358949dacf50671cd5d28e1ef702fb0
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-01-04 20:11:34 +0530

    Updated

--- src/Utils/Awards.js
+++ src/Utils/Awards.js
@@ -1,5 +1,5 @@
 export const awards = [
-  { id: 1, title: "Best Project Award", year: "Nov  2024" },
-  { id: 2, title: "Hackathon Finalist", year: "Feb  2025" },
-  { id: 3, title: "Modern Designer", year: "Aug  2025" },
+  { id: 1, title: "Best Project", year: "Nov  2024" },
+  { id: 2, title: "Resposnive Designer", year: "Feb  2025" },
+  { id: 3, title: "Modern Web Design", year: "Aug  2025" },
 ];
