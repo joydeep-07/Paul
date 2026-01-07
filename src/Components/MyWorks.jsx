f0d40d2f78f44335559e644e580a8895b456e6a5
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-01-07 12:16:23 +0530

    Skeleton Loader For images Applied

--- src/Components/MyWorks.jsx
+++ src/Components/MyWorks.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { projects } from "../Utils/Projects";
 import { useNavigate } from "react-router-dom";
 
 const MyWorks = () => {
+  const [loadedImages, setLoadedImages] = useState({});
   const navigate = useNavigate();
 
+  const handleImageLoad = (id) => {
+    setLoadedImages((prev) => ({ ...prev, [id]: true }));
+  };
+
   return (
-    <div className=" bg-[var(--bg-main)] transition-colors duration-300">
+    <div className="bg-[var(--bg-main)] transition-colors duration-300">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 sm:py-16">
         {/* PROJECT GRID */}
         <div className="grid gap-6 sm:gap-8 grid-cols-1 md:grid-cols-2">
@@ -27,12 +32,22 @@ const MyWorks = () => {
                 className="relative overflow-hidden rounded-2xl m-4
                 h-[220px] sm:h-[280px] md:h-[320px] lg:h-[380px]"
               >
+                {/* Skeleton */}
+                {!loadedImages[project.id] && (
+                  <div className="absolute inset-0 rounded-xl bg-[var(--border-light)] animate-pulse" />
+                )}
+
+                {/* Image */}
                 <img
                   src={project.thumbnail}
                   alt={project.title}
                   loading="lazy"
-                  className="w-full h-full object-contain rounded-xl
-                    transition-transform duration-700 "
+                  onLoad={() => handleImageLoad(project.id)}
+                  className={`
+                    w-full h-full object-contain rounded-xl
+                    transition-opacity duration-500
+                    ${loadedImages[project.id] ? "opacity-100" : "opacity-0"}
+                  `}
                 />
               </div>
 
@@ -60,10 +75,8 @@ const MyWorks = () => {
           <button
             onClick={() => navigate("/projects")}
             className="
-             sm:w-auto
               px-8 sm:px-10 md:px-12
               py-3 sm:py-3.5 md:py-4
-              min-h-[44px]
               rounded-full
               tracking-[0.12em]
               text-[10px] sm:text-xs uppercase
