ce74ee94d0545b463294704e226e5796cf5dbb65
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-01-13 09:46:49 +0530

    Updated

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
-import Footer from "./Footer";
 import ScrollToTop from "../Components/ScrollToTop";
-import AnimatedOutlet from "../Components/AnimatedOutlet";
 
 const Root = () => {
   return (
     <div className="min-h-screen bg-[var(--bg-main)] text-[var(--text-main)] transition-all duration-300">
       <Navbar />
-      <ScrollToTop/>
+      <ScrollToTop />
       <div className="pt-25">
         <Outlet />
-        {/* <AnimatedOutlet/> */}
       </div>
-      {/* <Footer /> */}
     </div>
   );
 };
