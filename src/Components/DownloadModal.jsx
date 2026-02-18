4b90a4fa052969bbe3b99088194aef52d487c749
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-02-18 09:38:16 +0530

    Upadted Ui

--- src/Components/DownloadModal.jsx
+++ src/Components/DownloadModal.jsx
@@ -3,11 +3,9 @@ import { DownloadCloud } from "lucide-react";
 export default function DownloadModal({ onCancel, onConfirm }) {
   return (
     <div className="flex flex-col items-center bg-[var(--bg-main)] shadow-md rounded-sm py-6 px-5 md:w-[460px] w-[370px] border border-[var(--border-light)]/40 ">
-    
-        <div className="flex items-center justify-center p-4 bg-[var(--accent-primary)]/10 border border-[var(--border-light)] rounded-full">
-          <DownloadCloud className=" text-[var(--accent-primary)] " />
-        </div>
-    
+      <div className="flex items-center justify-center p-4 bg-[var(--accent-primary)]/10 border border-[var(--border-light)] rounded-full">
+        <DownloadCloud className=" text-[var(--accent-primary)] " />
+      </div>
 
       <h2 className="text-[var(--text-main)] font-semibold mt-4 text-xl">
         Are you sure?
@@ -22,14 +20,14 @@ export default function DownloadModal({ onCancel, onConfirm }) {
       <div className="flex items-center justify-center gap-4 mt-5 w-full">
         <button
           onClick={onCancel}
-          className="w-full md:w-36 h-10 rounded-sm border border-[var(--border-light)] bg-[var(--text-main)] text-[var(--bg-main)] font-medium text-sm "
+          className="w-full cursor-pointer md:w-36 h-10 rounded-sm border border-[var(--border-light)] bg-[var(--text-main)] text-[var(--bg-main)] font-medium text-sm "
         >
           Cancel
         </button>
 
         <button
           onClick={onConfirm}
-          className="w-full md:w-36 h-10 rounded-sm text-white bg-green-600 font-medium text-sm hover:bg-green-700"
+          className="w-full cursor-pointer md:w-36 h-10 rounded-sm text-white bg-green-600 font-medium text-sm hover:bg-green-700"
         >
           Confirm
         </button>
