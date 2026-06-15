2088e7afcef4af0a36771f7954a2e432a462bee9
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-06-15 11:44:54 +0530

    Undo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Root from "./layout/Root";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
@@ -8,17 +8,14 @@ import { useSelector } from "react-redux";
 import Projects from "./Pages/Projects";
 import ReviewForm from "./Pages/ReviewForm";
 import ProjectDetails from "./Pages/ProjectDetails";
-import { Toaster } from "sonner";
+import { Toaster, toast } from "sonner";
 import Blogs from "./Pages/Blogs";
 import MernArchitectire from "./blog/AllBlogs/MernArchitectire";
 import AdvanceTailwind from "./blog/AllBlogs/AdvanceTailwind";
 import Python from "./blog/AllBlogs/Python";
-import PreLoader from "./components/PreLoader";
-import { AnimatePresence } from "framer-motion";
 
 const App = () => {
   const { mode } = useSelector((state) => state.theme);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (mode === "dark") {
@@ -27,14 +24,6 @@ const App = () => {
       document.documentElement.classList.remove("dark");
     }
   }, [mode]);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2400);
-
-    return () => clearTimeout(timer);
-  }, []);
 
   const router = createBrowserRouter([
     {
@@ -48,21 +37,18 @@ const App = () => {
         { path: "/blogs", element: <Blogs /> },
         { path: "/review/form", element: <ReviewForm /> },
         { path: "project/:id", element: <ProjectDetails /> },
+        // BLOG ROUTES
         { path: "blog/mern-architecture", element: <MernArchitectire /> },
         { path: "blog/advance-tailwind", element: <AdvanceTailwind /> },
         { path: "blog/python", element: <Python /> },
+
       ],
     },
   ]);
 
   return (
     <>
-      <AnimatePresence mode="wait">
-        {loading && <PreLoader key="preloader" />}
-      </AnimatePresence>
-
       <RouterProvider router={router} />
-
       <Toaster position="top-right" richColors />
     </>
   );
